ed296b3ea072eecde01b1d3d236b43eff6dbdd9f
Kevin Berridge <kberridge@pointeblank.net>
2012-01-22 13:34:39 -0500

    psake: added build, test, default tasks

--- default.ps1
+++ default.ps1
@@ -1,3 +1,18 @@
+$psake.context.Peek().config.framework = '4.0x86'
+
+properties {
+  $BuildConfiguration = 'Debug'
+}
+
+Task default -Depends Build, Test
+
+Task Build {
+  msbuild SimpleXml.sln /v:q /nologo /p:Configuration=$BuildConfiguration /t:Build
+}
+
+Task Test {
+  .\packages\NUnit.2.5.10.11092\tools\nunit-console.exe /nologo /framework=net-4.0 .\SimpleXmlTests\bin\Debug\SimpleXmlTests.dll
+}
 
 Task NuPack {
   pushd SimpleXml
